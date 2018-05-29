d59f8b3903ba67737a5c71199cb9651ecde9935f
201311113 <antjdlekd123@naver.com>
2018-05-29 19:36:47 +0900

    #1229 메인 클래스 구현

---
+++ src/main/java/se/smu/noid.java
@@ -0,0 +1,64 @@
+package se.smu;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.*;
+
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.table.DefaultTableModel;
+
+public class noid extends JFrame{
+
+	private JPanel contentPane;
+	private JTable table;
+	
+	public noid()
+	{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, 300, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(null);
+		setContentPane(contentPane);
+		//contentPane.setBackground(Color.WHITE);
+		setTitle("cancel");
+		
+JLabel NewLabel = new JLabel();
+		
+		NewLabel.setText("      Check imformation.");
+		NewLabel.setOpaque(true);
+		//NewLabel.setBackground(Color.WHITE);
+		NewLabel.setFont(new Font("µ¸À½", Font.PLAIN, 25));
+		NewLabel.setBounds(0, 50, 400, 50);
+		contentPane.add(NewLabel);
+		
+		JButton btnNewButton = new JButton("OK");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							login frame = new login();
+							frame.setVisible(true);
+							
+							setVisible(false);
+						    dispose();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		});
+		btnNewButton.setBounds(50, 130, 200, 50);
+		btnNewButton.setBackground(Color.BLUE);
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.setFont(new Font("µ¸À½", Font.PLAIN, 15));
+		contentPane.add(btnNewButton);
+	}
+}
+
+
+
+
