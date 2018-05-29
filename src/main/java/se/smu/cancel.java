f7a3165454a87a135ba3390d9b3d2df59813667f
201311113 <antjdlekd123@naver.com>
2018-05-29 19:19:23 +0900

    #1303 취소창 클래스 구현

---
+++ src/main/java/se/smu/cancel.java
@@ -0,0 +1,60 @@
+package se.smu;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.*;
+
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.table.DefaultTableModel;
+
+public class cancel extends JFrame{
+
+	private JPanel contentPane;
+	private JTable table;
+	
+	public cancel()
+	{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, 400, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(null);
+		setContentPane(contentPane);
+		//contentPane.setBackground(Color.WHITE);
+		setTitle("cancel");
+		
+JLabel NewLabel = new JLabel();
+		
+		NewLabel.setText("          취소되었습니다.");
+		NewLabel.setOpaque(true);
+		//NewLabel.setBackground(Color.WHITE);
+		NewLabel.setFont(new Font("돋음", Font.PLAIN, 30));
+		NewLabel.setBounds(0, 50, 400, 50);
+		contentPane.add(NewLabel);
+		
+		JButton btnNewButton = new JButton("OK");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							main frame = new main();
+							frame.setVisible(true);
+							
+							setVisible(false);
+						    dispose();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		});
+		btnNewButton.setBounds(85, 130, 200, 50);
+		btnNewButton.setBackground(Color.BLUE);
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.setFont(new Font("돋음", Font.PLAIN, 15));
+		contentPane.add(btnNewButton);
+	}
+}
